c553a62861a801aa9c01cd1926ff81516e64e72c
李佳帆 <3151342895@qq.com>
2023-03-09 23:20:56 +0800

    wip: support logout

---
+++ src/main/java/com/ojhelper/back/controller/basicInfo/BasicInfoController.java
@@ -0,0 +1,11 @@
+package com.ojhelper.back.controller.basicInfo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin
+@RequestMapping("/info")
+@Controller
+public class BasicInfoController {
+}
